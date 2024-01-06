c7257080d80f16c37ab60ea3d7e7b2d896d30a6f
Kework K. Kalustian <56342591+KewKalustian@users.noreply.github.com>
2024-01-06 11:28:06 +0100

    Update app.R

--- app.R
+++ app.R
@@ -6,29 +6,31 @@ library(shinyjs)
 library(shinyWidgets)
 
 
-js_magic <- c(
-  "table.on('key', function(e, datatable, key, cell, originalEvent){",
-  "  var targetName = originalEvent.target.localName;",
-  "  if(key == 13 && targetName == 'body'){",
-  "    $(cell.node()).trigger('dblclick.dt');",
-  "  }",
-  "});",
-  "table.on('keydown', function(e){",
-  "  var keys = [9,13,37,38,39,40];",
-  "  if(e.target.localName == 'input' && keys.indexOf(e.keyCode) > -1){",
-  "    $(e.target).trigger('blur');",
-  "  }",
-  "});",
-  "table.on('key-focus', function(e, datatable, cell, originalEvent){",
-  "  var targetName = originalEvent.target.localName;",
-  "  var type = originalEvent.type;",
-  "  if(type == 'keydown' && targetName == 'input'){",
-  "    if([9,37,38,39,40].indexOf(originalEvent.keyCode) > -1){",
-  "      $(cell.node()).trigger('dblclick.dt');",
-  "    }",
-  "  }",
-  "});"
+
+js_magic <- paste0(
+  "table.on('key', function(e, datatable, key, cell, originalEvent){
+    var targetName = originalEvent.target.localName;
+    if(key == 13 && targetName == 'body'){
+      $(cell.node()).trigger('dblclick.dt');
+    }
+  });
+  table.on('keydown', function(e){
+    var keys = [9,13,37,38,39,40];
+    if(e.target.localName == 'input' && keys.indexOf(e.keyCode) > -1){
+      $(e.target).trigger('blur');
+    }
+  });
+  table.on('key-focus', function(e, datatable, cell, originalEvent){
+    var targetName = originalEvent.target.localName;
+    var type = originalEvent.type;
+    if(type == 'keydown' && targetName == 'input'){
+      if([9,37,38,39,40].indexOf(originalEvent.keyCode) > -1){
+        $(cell.node()).trigger('dblclick.dt');
+      }
+    }
+  });"
 )
+
 
 
 
